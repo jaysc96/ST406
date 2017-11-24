b7dc9d623e765a3f35a59df5f010b75f7e9a0cbe
tstokvis <tmstokvis@gmail.com>
2017-11-24 13:10:45 -0800

    added test data

--- scripts/game_ops_script.sql
+++ scripts/game_ops_script.sql
@@ -1,22 +1,38 @@
-SELECT YEAR_ID, GAME_ID, BAT_ID,
+SELECT YEAR_ID, GAME_DATE, BAT_ID,
 		(AB*(H+BB+HBP)+TB*(AB+BB+SF+HBP))/(AB*(AB+BB+SF+HBP)) as OPS,
         H as HITS,
         AB as AT_BATS,
         BB as WALKS,
         HBP as HIT_BY_PITCH,
-        SF as SACRIFICE_FLYS
+        SF as SACRIFICE_FLYS,
+        SINGLES,
+        DOUBLES,
+        TRIPLES,
+        HOME_RUNS,
+        RBIS,
+        STRIKEOUTS
 FROM (SELECT 
       	YEAR_ID, 
-	GAME_ID,
+		substring(GAME_ID,8,4) as GAME_DATE,
         BAT_ID,
         sum(case when AB_FL = 'T' then 1 else 0 end) as AB,
         sum(case when H_CD > 0 then 1 else 0 end) as H,
         sum(case when EVENT_CD = 16 then 1 else 0 end) as HBP,
         sum(case when SF_FL = 'T' then 1 else 0 end) as SF,
         sum(case when EVENT_CD = 14 then 1 else 0 end) as BB,
-        sum(H_CD) as TB
+        sum(H_CD) as TB,
+        sum(case when EVENT_CD = 20 then 1 else 0 end) as SINGLES,
+        sum(case when EVENT_CD = 21 then 1 else 0 end) as DOUBLES,
+        sum(case when EVENT_CD = 22 then 1 else 0 end) as TRIPLES,
+        sum(case when EVENT_CD = 23 then 1 else 0 end) as HOME_RUNS,
+        sum(RBI_CT) as RBIS,
+        sum(case when BATTEDBALL_CD = 'F' then 1 else 0 end) as FLYBALLS,
+        sum(case when BATTEDBALL_CD = 'L' then 1 else 0 end) as LINEDRIVES,
+        sum(case when BATTEDBALL_CD = 'P' then 1 else 0 end) as POPUPS,
+        sum(case when BATTEDBALL_CD = 'G' then 1 else 0 end) as GROUNDBALLS,
+        sum(case when EVENT_CD = 3 then 1 else 0 end) as STRIKEOUTS
 	FROM retrosheet.events
-	WHERE YEAR_ID = 2016 # <-- Select Year 
+	WHERE YEAR_ID > 2010 AND YEAR_ID != 2016 # <-- Select Year 
 	group by GAME_ID, BAT_ID) game_query
 having AT_BATS > 0
-order by BAT_ID;
+order by YEAR_ID,BAT_ID;
